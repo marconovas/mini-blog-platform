f646e2258cb25366c13973d0959304f53ba0853d
Marco Novas <marco.nicolas.novas@gmail.com>
2026-05-21 19:10:43 -0300

    first commit: api fetch and basic structure

---
+++ src/services/posts.service.js
@@ -0,0 +1,81 @@
+
+const API_URL = "http://localhost:3000/posts";
+
+export async function getPosts() {
+    const res = await fetch(API_URL);
+
+    if(!res.ok){
+        throw new Error(res.message);
+    }
+
+    return res.json();
+}
+
+export async function getPostById (id) {
+    const res = await fetch(`${API_URL}/${id}`);
+
+    if(!res.ok){
+        const errorData = await res.json();
+
+        throw new Error(errorData.message);
+    }
+
+    return res.json();
+}
+
+export async function createPost(title, content) {
+
+    const res = await fetch(API_URL,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title,
+            content
+        })
+    });
+
+    if(!res.ok){
+        const errorData = await res.json();
+
+        throw new Error(errorData.message);
+    }
+
+    return res.json();
+}
+
+export async function editPost(id, title, content) {
+    const res  = await fetch(`${API_URL}/${id}`,{
+        method: "PUT",
+        headers: {
+            "Content-type" : "application/json"
+        },
+        body: JSON.stringify({
+            title,
+            content
+        })
+    })
+
+    if(!res.ok){
+        const errorData = await res.json();
+
+        throw new Error(errorData.message);
+    }
+
+    return res.json();
+}
+
+export async function deletePost(id) {
+    const res = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE"
+    })
+
+    if(!res.ok){
+        const errorData = await res.json();
+
+        throw new Error(errorData.message);
+    }
+
+    return res.json();
+}
