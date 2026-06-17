57a52637c5ab6498e2b84d8b230127b3f3464e3e
Marco Novas <marco.nicolas.novas@gmail.com>
2026-06-17 00:28:20 -0300

    fix auth routes

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,5 @@
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = `${import.meta.env.VITE_API_URL}/auth`
 
 export async function userRegister(name, email, password) {
     const userData = await fetch(`${API_URL}/register`, {
